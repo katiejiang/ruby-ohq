291a181cd8004d4363c8e952e18e9ebdda2a3eb2
Katie Jiang <kajiang@seas.upenn.edu>
2018-04-19 16:41:48 -0400

    Show question view

--- app/models/question.rb
+++ app/models/question.rb
@@ -10,4 +10,9 @@ class Question < ApplicationRecord
     return 'positive' if status == 'Being helped'
     return 'warning' if status == 'Waiting'
   end
+
+  def ribbon_color
+    return 'green' if status == 'Being helped'
+    return 'yellow' if status == 'Waiting'
+  end
 end
