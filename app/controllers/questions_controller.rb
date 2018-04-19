00ddfc069a9cb4c89c1b82cdbd78bebc73b74120
Katie Jiang <kajiang@seas.upenn.edu>
2018-04-19 13:42:17 -0400

    New question under course

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,7 @@
 class QuestionsController < ApplicationController
   before_action :set_question, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user
+  before_action :set_course
 
   # GET /questions
   # GET /questions.json
@@ -27,14 +28,10 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new(question_params)
 
-    respond_to do |format|
-      if @question.save
-        format.html { redirect_to @question, notice: 'Question was successfully created.' }
-        format.json { render :show, status: :created, location: @question }
-      else
-        format.html { render :new }
-        format.json { render json: @question.errors, status: :unprocessable_entity }
-      end
+    if @question.save
+      redirect_to @course
+    else
+      render :new
     end
   end
 
@@ -55,17 +52,18 @@ class QuestionsController < ApplicationController
   # DELETE /questions/1
   # DELETE /questions/1.json
   def destroy
-    @question.destroy
-    respond_to do |format|
-      format.html { redirect_to questions_url, notice: 'Question was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    @question.destroy if @question.user == current_user
+    redirect_back(fallback_location: @course)
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_question
       @question = Question.find(params[:id])
+    end
+
+    def set_course
+      @course = Course.find(params[:course_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  resources :questions
-  resources :courses
+  resources :courses do
+    resources :questions
+  end
   resources :users, except: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htmlc
 end
