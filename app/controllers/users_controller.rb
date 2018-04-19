59aa97b6adc3abb67e1260d7ee8538064228d844
Katie Jiang <kajiang@seas.upenn.edu>
2018-04-19 01:41:57 -0400

    Destroy user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,11 +45,11 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
+    if @user == current_user
+      @user.destroy
+      reset_session
     end
+    redirect_to root_path
   end
 
   private
