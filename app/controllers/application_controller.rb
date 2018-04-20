220b48ba8e0e1eeeb2f66cc84e04308bb28c0d2b
Katie Jiang <kajiang@seas.upenn.edu>
2018-04-20 16:58:14 -0400

    Add nav bar search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :logged_in?, :current_user
+  before_action :search_contents
 
   private
+
+  def search_contents
+    courses = Course.all.map { |course| { 'category': 'Course', 'link': "/courses/#{course.id}", 'title': course.name } }
+    names = User.all.map { |user| { 'category': 'User name', 'link': "/users/#{user.id}", 'title': user.name } }
+    emails = User.all.map { |user| { 'category': 'User email', 'link': "/users/#{user.id}", 'title': user.email } }
+    @search_contents = (courses + names + emails).to_json.to_s.html_safe
+  end
 
   def logged_in?
     session[:user_id]
