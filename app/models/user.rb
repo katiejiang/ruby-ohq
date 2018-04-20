ca337b87e5f689248f4639925a70be7172f28e28
Katie Jiang <kajiang@seas.upenn.edu>
2018-04-19 22:31:10 -0400

    Add instructor/student courses

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :staffs, dependent: :destroy
   has_many :questions, dependent: :destroy
 
+  has_many :student_courses, dependent: :destroy, through: :students, source: :course
+  has_many :instructor_courses, dependent: :destroy, through: :staffs, source: :course
+
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
@@ -23,5 +26,34 @@ class User < ApplicationRecord
 
   def is_student?(course)
     return !Student.find_by(user: self, course: course).nil?
+  end
+
+  def enroll
+    Student.create(user: user, course: self, favorite: false)
+  end
+
+  def unenroll
+    student = Student.create(user: user, course: self)
+    student.destroy if student
+  end
+
+  def favorite(course)
+    student = Student.find_by(user: self, course: course)
+    return unless student
+    student.update(favorite: true)
+  end
+
+  def unfavorite(course)
+    student = Student.find_by(user: self, course: course)
+    return unless student
+    student.update(favorite: false)
+  end
+
+  def favorite_courses
+    (students.select{ |student| student[:favorite] }).map{ |student| student.course }
+  end
+
+  def nonfavorite_courses
+    (students.select{ |student| !student[:favorite] }).map{ |student| student.course }
   end
 end
