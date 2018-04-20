507c3a16011a11b5ff238422faf3e82844a3d1bb
Katie Jiang <kajiang@seas.upenn.edu>
2018-04-20 00:11:54 -0400

    Add permission_reuiqred to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, except: [:index, :new, :create]
   before_action :authenticate_user, except: [:new, :create]
+  before_action :permission_required, only: [:show, :edit, :update, :destroy]
 
   # GET /users/1
   # GET /users/1.json
@@ -16,7 +17,6 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
-    redirect_to root_path unless @user == current_user
   end
 
   # POST /users
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    if @user == current_user && @user.update(user_params)
+    if @user.update(user_params)
       session[:user_id] = @user.id
       redirect_to @user
     else
@@ -45,17 +45,22 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    if @user == current_user
-      @user.destroy
-      reset_session
-    end
+    @user.destroy
+    reset_session
     redirect_to root_path
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      @user = User.find_by(id: params[:id])
+    end
+
+    def permission_required
+      unless @user == current_user
+        redirect_to current_user
+        return
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
